3485924c52d94091e55ad51e061fb77f9cba51fd
David Morgenstern <DavidtheConfident@gmail.com>
2021-10-14 14:30:00 -0500

    commit tree a7d0954e6d6e629fad2cd8fe28048861d04214ed

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Code your solution here
+/*function findMatching(drivers, str){
+    drivers.filter(function(str2){
+        if(str === str2){
+            return 
+        }
+    })
+}
+*/
+
+const findMatching = (drivers, str) => drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());;
+
+
+//function findMatching(drivers, str){
+   /* const arr = []
+    
+    for(let i = 0; i < drivers.length; i++){
+        if(drivers[i] === str){
+            arr.push(drivers[i]);
+        }
+    }
+    return arr;
+    */
+
+   /*return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+}*/
+
+const fuzzyMatch = (drivers, str) => drivers.filter(driver => driver.startsWith(str))
+
+/*function fuzzyMatch(drivers, str){
+    const arr = []
+    //let count = 0;
+
+    //const match = [...drivers];
+
+    for(let i = 0; i < drivers.length; i++){
+        //match[i] = match[i].split("", str.length);
+
+        if(drivers[i].startsWith(str))
+        {
+            arr.push(drivers[i])
+            //count++;
+        }
+
+    }
+    /*
+    if(count != 0){
+        return arr
+    }
+    
+   return arr
+}
+*/
+
+function matchName(drivers, str){
+    let car = [];
+
+    for(const driver of drivers){
+        if(driver.name === str){
+            car.push(driver)
+        }
+    }
+    return car;
+}
